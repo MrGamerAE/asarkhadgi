452d8639c93de472a390feb7d2e22d7c6ec15948
MrGamerAE <145361191+MrGamerAE@users.noreply.github.com>
2023-12-14 20:50:06 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // DOM is ready, you can interact with elements here
+
+    // Example: Display an alert when a button is clicked
+    const alertButton = document.getElementById("alertButton");
+
+    alertButton.addEventListener("click", function () {
+        alert("Hello from Asar Khadgi!");
+    });
+});
